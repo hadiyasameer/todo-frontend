4758171b776253c4e2afc95d2b1145e0b79f041e
hadiyasameer <hadiyasameer2013@gmail.com>
2025-04-23 02:30:50 +0530

    fix: change baseURL in axiosconfig  frontend

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://todo-backend-rbvn.onrender.com'
 });
 
 instance.interceptors.request.use((config) => {
