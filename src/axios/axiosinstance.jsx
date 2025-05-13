855e0f3bb142689426ab0497abbdbe113c1872ea
hadiyasameer <hadiyasameer2013@gmail.com>
2025-05-13 20:54:15 +0530

    updated axiosinstance and vite.config

--- src/axios/axiosinstance.jsx
+++ src/axios/axiosinstance.jsx
@@ -1,10 +1,11 @@
 import axios from 'axios'
 
-const url=import.meta.env.VITE_BASE_URL
-console.log(url,"baseurl")
-const axiosInstance = axios.create({
-    baseURL:url,
-    withCredentials:true
-})
+const url = import.meta.env.VITE_API_URL;
+console.log(url, "baseurl");
 
-export {axiosInstance}
+const axiosInstance = axios.create({
+    baseURL: url || 'http://localhost:3000',
+    withCredentials: true
+});
+
+export default axiosInstance;  
