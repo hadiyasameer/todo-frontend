1a1bda9db906a12911a1f77bd4c77befbd6053e2
hadiyasameer <hadiyasameer2013@gmail.com>
2025-05-13 21:47:35 +0530

    corrected deletedtodo url

--- src/pages/DeleteToDo.jsx
+++ src/pages/DeleteToDo.jsx
@@ -14,7 +14,7 @@ function DeleteToDo() {
 
     const handleDelete = (id) => {
         if (window.confirm('Are you sure you want to delete this item?')) {
-            axiosInstance.delete(`/todo/${id}`)
+            axiosInstance.delete(`/todo/todos/${id}`)
                 .then(() => {
                     setTodos(prev => prev.filter(todo => todo._id !== id));
                     alert('To-Do deleted successfully!');
