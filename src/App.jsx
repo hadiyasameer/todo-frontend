19b1d2d6c3e7235d279b1833e38609d98aab33b2
hadiyasameer <hadiyasameer2013@gmail.com>
2025-05-14 23:38:23 +0530

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,32 @@
-import React from 'react'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
-import Home from './pages/Home'
-import ShowToDo from './pages/ShowToDo'
-import EditToDo from './pages/EditToDo'
-import DeleteToDo from './pages/DeleteToDo'
+import React from 'react';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';  // Add this import
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { store, persister } from './redux/store';  // Ensure persister is imported
+
+import Home from './pages/Home';
+import ShowToDo from './pages/ShowToDo';
+import EditToDo from './pages/EditToDo';
+import DeleteToDo from './pages/DeleteToDo';
 import CreateTodo from './pages/CreateToDo';
 
 function App() {
   return (
-      <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/show/:id" element={<ShowToDo/>}></Route>
-        <Route path="/edit/:id" element={<EditToDo/>}></Route>
-        <Route path="/delete/:id" element={<DeleteToDo/>}></Route>
-        <Route path="/create" element={<CreateTodo />} />
-        
-      </Routes>
-      </BrowserRouter>
-  )
+    <Provider store={store}>
+      {/* Wrap the app with PersistGate and provide persister */}
+      <PersistGate loading={<div>Loading...</div>} persistor={persister}>  
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/show/:id" element={<ShowToDo />} />
+            <Route path="/edit/:id" element={<EditToDo />} />
+            <Route path="/delete/:id" element={<DeleteToDo />} />
+            <Route path="/create" element={<CreateTodo />} />
+          </Routes>
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  );
 }
 
-export default App
+export default App;
