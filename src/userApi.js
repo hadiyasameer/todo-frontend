e8c9f01e136e334d2b3576d16b17740c2ddb485e
hadiyasameer <hadiyasameer2013@gmail.com>
2025-05-16 03:45:17 +0530

    updated logout route in userapi.js

--- src/userApi.js
+++ src/userApi.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export const userLogout = async () => {
     const response = await axios.post(
-        `${import.meta.env.VITE_API_URL}/logout`,
+        `${import.meta.env.VITE_API_URL}/user/logout`,
         {},
         { withCredentials: true } 
     );
